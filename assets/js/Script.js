ecd6797851ea757b58d4785a7ad8a9b8fd4e95c0
ehtesham-ez <57299111+ehtesham-ez@users.noreply.github.com>
2021-01-27 22:43:44 +0330

    Home page and Files page | Javeelin

---
+++ assets/js/Script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    //Change Display of form collapse bottom to none
+    if($(window).width() > 992) {
+        $('.form-coll').addClass('d-none');
+        $('#collapseForm').addClass('show');
+    }
+
+});
